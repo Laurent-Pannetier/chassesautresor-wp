64b4b348e1965ce21ded18df8db05c3273b1a071
Laurent Pannetier <lpannetier74@gmail.com>
2025-06-15 18:41:57 +0200

    clean debug code from traitement template

--- templates/page-traitement-reponse.php
+++ templates/page-traitement-reponse.php
@@ -1,16 +1,7 @@
 <?php
-
 /**
  * Template Name: Traitement Réponse (Finalisation Sécurisée)
  */
-
-if (!function_exists('get_field')) {
-  require_once(ABSPATH . 'wp-load.php');
-}
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 defined('ABSPATH') || exit;
 
