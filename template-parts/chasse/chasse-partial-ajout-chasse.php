b8acbe570e634aabb9d7db6071bb90bc92e7866e
laurent <lpannetier74@gmail.com>
2025-06-21 21:26:28 +0200

    fix

---
+++ template-parts/chasse/chasse-partial-ajout-chasse.php
@@ -0,0 +1,24 @@
+<?php
+defined('ABSPATH') || exit;
+
+$organisateur_id = $args['organisateur_id'] ?? null;
+$has_chasses = $args['has_chasses'] ?? false;
+
+if (!$organisateur_id || get_post_type($organisateur_id) !== 'organisateur') {
+  return;
+}
+?>
+
+<article id="carte-ajout-chasse" class="carte-chasse carte-ajout-chasse disabled" data-post-id="0">
+  <div class="carte-chasse-contenu">
+    <div class="icone-ajout">
+      <i class="fa-solid fa-circle-plus fa-3x"></i>
+    </div>
+    <h2><?= $has_chasses ? 'Ajouter une nouvelle chasse' : 'Créer ma première chasse'; ?></h2>
+  </div>
+
+  <div class="overlay-message">
+    <i class="fa-solid fa-circle-info"></i>
+    <p>Complétez d’abord : titre, logo, description</p>
+  </div>
+</article>
