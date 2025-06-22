1e32ae58933bc69c2e5cd883255d3283941aa652
Laurent Pannetier <lpannetier74@gmail.com>
2025-06-22 06:13:36 +0200

    Add organizer hunts loop

---
+++ template-parts/organisateur/organisateur-partial-boucle-chasses.php
@@ -0,0 +1,22 @@
+<?php
+defined('ABSPATH') || exit;
+
+$organisateur_id = $args['organisateur_id'] ?? null;
+if (!$organisateur_id || get_post_type($organisateur_id) !== 'organisateur') {
+  return;
+}
+
+$query = get_chasses_de_organisateur($organisateur_id);
+$posts = is_a($query, 'WP_Query') ? $query->posts : (array) $query;
+?>
+
+<div class="grille-liste">
+  <?php foreach ($posts as $post) : ?>
+    <?php
+    $chasse_id = $post->ID;
+    get_template_part('template-parts/organisateur-partial-chasse-card', null, [
+      'chasse_id' => $chasse_id,
+    ]);
+    ?>
+  <?php endforeach; ?>
+</div>
